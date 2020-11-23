419c1c62562fdc1bd6b60259fecb02801e91d5aa
Jeremiah <newcreationjerry96@gmail.com>
2020-11-23 11:03:33 +0000

    wrote simple-bank.sol

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -3,6 +3,7 @@
     Breaking changes from 0.5 to 0.6 can be found here: 
     https://solidity.readthedocs.io/en/v0.6.12/060-breaking-changes.html
 */
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.12;
 
@@ -11,28 +12,28 @@ contract SimpleBank {
     //
     // State variables
     //
-    
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) balances;
+    mapping (address => uint) private balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address owner;
+    address public owner;
     
     //
     // Events - publicize actions to external listeners
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled();
+    event LogEnrolled(address accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
 
     //
@@ -42,6 +43,7 @@ contract SimpleBank {
     /* Use the appropriate global variable to get the sender of the transaction */
     constructor() public {
         /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     // Fallback function - Called if other functions don't match call or
@@ -52,19 +54,26 @@ contract SimpleBank {
     fallback() external payable {
         revert();
     }
-
+    
     /// @notice Get balance
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
         /* Get the balance of the sender of this transaction */
+         balances[msg.sender];
+         return msg.sender.balance;
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
     function enroll() public returns (bool){
+        
+        emit LogEnrolled(msg.sender);
+        return enrolled[msg.sender] = true;
+         
+         
     }
 
     /// @notice Deposit ether into bank
@@ -73,9 +82,16 @@ contract SimpleBank {
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event    
     // Users should be enrolled before they can make deposits
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
+          address depositor = msg.sender;
+          require(enrolled[depositor], 'this address is not enrolled');
+          uint depositAmount = msg.value;
+          uint currentBalance = getBalance() + depositAmount;
+          currentBalance = balances[msg.sender];
+          emit LogDepositMade(depositor, depositAmount);
+          return currentBalance;
     }
 
     /// @notice Withdraw ether from bank
@@ -88,6 +104,14 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
+           address accountAddress = msg.sender;
+           require(balances[accountAddress] > withdrawAmount);
+           msg.sender.transfer(withdrawAmount);
+           uint newBalance = balances[accountAddress] - withdrawAmount;
+           newBalance = balances[accountAddress];
+           emit LogWithdrawal(accountAddress, withdrawAmount, newBalance);
+           return newBalance;
+           
     }
 
 }
--- truffle-config.js
+++ truffle-config.js
@@ -8,7 +8,8 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: "*" ,// Match any network id
+      networkCheckTimeout: 1000000000
     }
   }
 };
